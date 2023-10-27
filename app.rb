83fb723e364da27395d93def8e9d5f181218cabe
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-10-27 23:33:38 +0000

    dice roll from git

--- app.rb
+++ app.rb
@@ -2,8 +2,60 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/dynamic/:number_of_dice/6") do
+  @num_dice = params.fetch("number_of_dice").to_i
+
+  @rolls = []
+
+  @num_dice.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+	second_die = rand(1..6)
+  sum = first_die + second_die
+	
+	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+	erb(:two_six)
+end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+	second_die = rand(1..10)
+  sum = first_die + second_die
+	
+	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  erb(:two_ten)
+end
+
+get("/dice/1/20") do
+  @die = rand(1..20)
+	
+	@outcome = "You rolled a #{@die}."
+	
+	erb(:one_twenty)
+end
+
+get("/dice/5/4") do
+  first_die = rand(1..4)
+	second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  fifth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+	
+	@outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}."
+	
+  erb(:five_four)
 end
