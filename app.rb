dfff19f05f2a85dce2b0a1004d0e28d2493c0862
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-10-28 13:59:21 +0000

    list rolls

--- app.rb
+++ app.rb
@@ -9,6 +9,12 @@ get("/process_roll") do
   @num_dice = params.fetch("dice").to_i
   @num_sides = params.fetch("sides").to_i
 
+  @rolls = []
+  
+  @num_dice.times do
+    @rolls.push(rand(1..@num_sides))
+  end
+
   erb(:results)
 end
 
